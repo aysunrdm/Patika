ea3a495483b7975230738137df3641d6b8b0c8d7
aysunrdm <aysunerdem93@gmail.com>
2021-07-28 00:33:03 +0300

    Patika ödev

---
+++ src/TriangleAreaCalculation/TriangleAreaCal.java
@@ -0,0 +1,24 @@
+package TriangleAreaCalculation;
+import java.lang.Math;
+import java.util.Scanner;
+
+public class TriangleAreaCal{
+    public static void main(String[] args){
+        
+        double kenar1, kenar2, kenar3, alan;
+
+        Scanner input = new Scanner(System.in);
+
+        System.out.println("Alanını hesaplamak istediğiniz üçgenin kenar uzunluklarını sırasıyla giriniz: ");
+        kenar1=input.nextDouble();
+        kenar2=input.nextDouble();
+        kenar3=input.nextDouble();
+
+        double 𝑢 = (kenar1+kenar2+kenar3) / 2;
+        
+        alan = Math.sqrt(𝑢 * (𝑢 - kenar1) * (𝑢 - kenar2) * (𝑢 - kenar3));
+        System.out.println("Alan: " + alan);
+        
+    }
+
+}
