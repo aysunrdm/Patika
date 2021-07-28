835013d6aac4cb0bc72c5e8c59d3684cdeb1a8a8
aysunrdm <aysunerdem93@gmail.com>
2021-07-29 01:12:39 +0300

    patika taximeter

--- src/Taximeter/taximeter.java
+++ src/Taximeter/taximeter.java
@@ -1,11 +1,12 @@
 package Taximeter;
+
 import java.util.Scanner;
 
 public class taximeter {
     public static void main(String[] args){
 
-    int km;
     double perKm = 2.20, total, startPrice = 10;
+    int km;
 
     Scanner input=new Scanner(System.in);
     System.out.print("Mesafeyi KM cinsinden giriniz: ");
