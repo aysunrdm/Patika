fc4742e884812d4b038500f965f0463053e9aa30
aysunrdm <aysunerdem93@gmail.com>
2021-07-30 16:45:26 +0300

    BodyMassIndex

---
+++ src/BodyMassIndex/BodyMassIndex.java
@@ -0,0 +1,23 @@
+package BodyMassIndex;
+import java.util.Scanner;
+
+public class BodyMassIndex {
+
+    public static void main(String[] args){
+
+    Scanner input = new Scanner(System.in);
+
+    int weight;
+    double bodyMassIndex, height;
+    
+    System.out.print("Lütfen boyunuzu (metre cinsinde) giriniz :");
+    height = input.nextDouble();
+    System.out.print("Lütfen kilonuzu giriniz :");
+    weight = input.nextInt();
+    
+    bodyMassIndex = weight / (height * height);
+
+    System.out.println("Vücut Kitle İndeksiniz :" + bodyMassIndex);
+
+    }    
+}
