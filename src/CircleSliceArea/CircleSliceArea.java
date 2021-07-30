523057668bc3ce4b0f3eace5e5bc6fe2da246ec2
aysunrdm <aysunerdem93@gmail.com>
2021-07-30 13:39:15 +0300

    CircleSliceArea

---
+++ src/CircleSliceArea/CircleSliceArea.java
@@ -0,0 +1,24 @@
+package CircleSliceArea;
+import java.util.Scanner;
+
+public class CircleSliceArea {
+    
+public static void main(String[] args){
+
+Scanner input = new Scanner(System.in);
+
+    int r, 𝛼;
+    double pi=3.14, sliceArea;
+
+    System.out.println("Yarıçapı ve merkez açısının ölçüsünü giriniz: ");
+    
+    r= input.nextInt();
+    𝛼= input.nextInt();
+
+    sliceArea = (pi * (r*r) * 𝛼) / 360;
+
+    System.out.println("Daire diliminin alanı: " + sliceArea);
+
+    
+}
+}
