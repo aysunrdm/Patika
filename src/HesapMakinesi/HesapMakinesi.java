0e512e46b612a82ab13b6608fdb0bfb2ea913f1a
aysunrdm <aysunerdem93@gmail.com>
2021-08-01 02:15:45 +0300

    Patika

---
+++ src/HesapMakinesi/HesapMakinesi.java
@@ -0,0 +1,55 @@
+package HesapMakinesi;
+
+import java.util.Scanner;
+
+public class HesapMakinesi {
+
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        int n1, n2, select;
+
+        System.out.println("İlk Sayıyı Giriniz: ");
+        n1 = sc.nextInt();
+
+        System.out.println("İkinci Sayıyı Giriniz: ");
+        n2 = sc.nextInt();
+
+        System.out.println("1-Toplama\n2-Çıkarma\n3-Çarpma\n4-Bölme");
+        select = sc.nextInt();
+
+        sc.close();
+
+        switch (select) {
+            case 1:
+
+                System.out.println("Toplam : " + (n1 + n2));
+                break;
+
+            case 2:
+
+                System.out.println("Çıkarma : " + (n1 - n2));
+                break;
+
+            case 3:
+
+                System.out.println("Çarpma : " + (n1 * n2));
+                break;
+
+            case 4:
+
+                if (n2 != 0)
+                    System.out.println("Bölme : " + (n1 / n2));
+
+                else
+                    System.out.println("Bir sayı 0'a bölünemez !");
+                break;
+
+            default:
+                System.out.println("Yanlış seçim yaptınız. Tekrar deneyiniz.");
+                break;
+        }
+
+    }
+}
